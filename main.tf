12e74a5774913e2a0b96fccdf30ea7346b75da9b
sarath8008 <sarath.paluri333@gmail.com>
2026-02-27 21:16:02 +0530

    Add EC2 module code

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  user_data              = var.user_data
+
+  tags = {
+    Name = var.instance_name
+  }
+}
