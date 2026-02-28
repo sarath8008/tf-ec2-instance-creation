c21c0ac844f10408fe00ce5f459df9a2b1431977
sarath8008 <sarath.paluri333@gmail.com>
2026-02-28 09:31:50 +0530

    Add variables definitions

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+
+variable "user_data" {
+  description = "User data script"
+  type        = string
+  default     = ""
+}
